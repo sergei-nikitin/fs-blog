641ff3223a57218177b98207f0831dc242d1bf1c
sergei-nikitin <s.dm.nikitin@gmail.com>
2022-10-11 21:00:23 +0300

    11.10.22

---
+++ utils/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+
+  if (token) {
+    const decoded = jwt.verify(token, 'secretKeyForToken');
+
+    req.userId = decoded._id;
+    try {
+    } catch (error) {
+      return res.status(403).json({
+        message: 'Нет доступа',
+      });
+    }
+  } else {
+    return res.status(403).json({
+      message: 'Нет доступа',
+    });
+  }
+
+  next();
+};
